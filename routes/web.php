11d7e8c3cb932d6e6eadd583e06d2d6a9717dba7
MohsenRkia <mohsenrostamikia@gmail.com>
2020-03-01 21:44:28 +0330

    Installin Laravel and First HTML for Site and AdminPanel

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/* Website Routs */
+Route::name('site.index')->get('/', 'HomesiteController@index');
+
+
+
+
+
+
+/* Admin Panel */
+Route::prefix('admin')->group(function (){
+    Route::name('admin.index')->get('/dashboard','AdminhomeController@index');
+
+});
